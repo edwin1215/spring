e090f84d1c28294ee643f0c59ad7f51cdd095c2c
Edwin <Edwin@Edwin>
2016-10-11 23:33:53 +0800

    methodInterceptor
    Signed-off-by: Edwin <Edwin@Edwin>

---
+++ spring-web/src/main/java/com/edwin/spring/web/aop/advice/surround/GreetingInterceptor.java
@@ -0,0 +1,16 @@
+package com.edwin.spring.web.aop.advice.surround;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+public class GreetingInterceptor implements MethodInterceptor {
+
+	@Override
+	public Object invoke(MethodInvocation invocation) throws Throwable {
+		Object[] args = invocation.getArguments();
+		String name = (String) args[0];
+		System.out.println(name);
+		return null;
+	}
+
+}
