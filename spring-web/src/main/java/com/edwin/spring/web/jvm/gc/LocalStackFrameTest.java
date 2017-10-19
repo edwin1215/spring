ff967f0c52ef1304d063ab64cccf93beca7146fd
edwin1215 <junming.cao@busonline.com>
2017-10-19 17:52:17 +0800

    Signed-off-by: edwin1215 <junming.cao@busonline.com>

---
+++ spring-web/src/main/java/com/edwin/spring/web/jvm/gc/LocalStackFrameTest.java
@@ -0,0 +1,20 @@
+package com.edwin.spring.web.jvm.gc;
+
+/**
+ * 局部变量表GC
+ * <-verbose:gc>
+ * 
+ * @author caojunming
+ */
+public class LocalStackFrameTest {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		{
+			byte[] placeholder = new byte[64 * 1024 * 1024];
+			// placeholder = null;
+		}
+		int i = 0;
+		System.gc();
+	}
+}
