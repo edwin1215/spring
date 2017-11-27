f96c6acf91d0041544f0519b463e91012aba7ec6
edwin1215 <junming.cao@busonline.com>
2017-11-27 20:24:56 +0800

    test case
    Signed-off-by: edwin1215 <junming.cao@busonline.com>

--- spring-web/src/main/java/com/edwin/spring/web/io/TestRandomAccessFile.java
+++ spring-web/src/main/java/com/edwin/spring/web/io/TestRandomAccessFile.java
@@ -5,6 +5,12 @@ import java.io.RandomAccessFile;
 
 public class TestRandomAccessFile {
   public static void main(String[] args) throws IOException {
+    String timeStr = String.valueOf(System.currentTimeMillis());
+    System.out.println(timeStr);
+    System.out.println(timeStr.substring(timeStr.length() - 6, timeStr.length()));
+  }
+
+  public static void deal() throws IOException {
     RandomAccessFile rf = new RandomAccessFile("D:/edwin/jvm/tmp/rtest.dat", "rw");
     for (int i = 0; i < 10; i++) {
       // 写入基本类型double数据
