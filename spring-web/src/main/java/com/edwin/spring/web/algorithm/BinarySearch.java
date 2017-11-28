ca98df6b40f3c9b3678129ba0abb63d3eaab6d6a
edwin1215 <junming.cao@busonline.com>
2017-11-28 15:30:44 +0800

    二分搜索范围查找、断言
    Signed-off-by: edwin1215 <junming.cao@busonline.com>

--- spring-web/src/main/java/com/edwin/spring/web/algorithm/BinarySearch.java
+++ spring-web/src/main/java/com/edwin/spring/web/algorithm/BinarySearch.java
@@ -11,50 +11,78 @@ public class BinarySearch {
   public static void main(String[] args) {
     int[] arr = {1, 3, 4, 5, 6, 8, 9, 10, 12, 15};
     System.out.println(getPosition(arr, 7));
-    int[] arrRep = {1, 3, 4, 4, 5, 5, 5, 6, 6, 8, 9,};
-    getRangePosition(arrRep, 5);
+    int[] arrRep = {1, 3, 4, 4, 5, 5, 5, 5, 5, 6, 6, 8, 9, 12};
+    getRangePosition(arrRep, 4);
 
   }
 
+  /**
+   * 无重复数据二分查找
+   * 
+   * @param arr
+   * @param a
+   * @return
+   */
   public static int getPosition(int[] arr, int a) {
-
-    int begin = 0;
-    int end = arr.length - 1;
-
-    while (begin <= end) {
-      int pos = begin + (end - begin) / 2;
-      System.out.println("----" + pos);
-      if (arr[pos] > a) {
-        end = pos - 1;
-      } else if (arr[pos] < a) {
-        begin = pos + 1;
+    int lp = 0;
+    int rp = arr.length - 1;
+    while (lp <= rp) {
+      int mid = lp + (rp - lp) / 2;
+      System.out.println("----" + mid);
+      if (arr[mid] > a) {
+        rp = mid - 1;
+      } else if (arr[mid] < a) {
+        lp = mid + 1;
       } else {
-        return pos;
+        return mid;
       }
     }
     return -1;
   }
 
+  /**
+   * 范围二分查找
+   * 
+   * @param arr
+   * @param a
+   */
   public static void getRangePosition(int[] arr, int a) {
+    // 整体范围指针
+    int lp = 0;
+    int rp = arr.length - 1;
+    // 相同值的范围指针
+    int low = lp;
+    int high = rp;
 
-    int begin = 0;
-    int end = arr.length - 1;
-    // int low = begin;
-    // int high = end;
-
-    while (begin <= end) {
-      int pos = begin + (end - begin) / 2;
-      System.out.println("----" + pos);
-      if (arr[pos] > a) {
-        end = pos - 1;
-        // high = end;
-      } else if (arr[pos] < a) {
-        begin = pos + 1;
-        // low = begin;
+    while (lp <= rp) {
+      int mid = lp + (rp - lp) / 2;
+      System.out.println("----" + mid);
+      if (arr[mid] > a) {
+        rp = mid - 1;
+      } else if (arr[mid] < a) {
+        lp = mid + 1;
       } else {
-        // return pos;
+        low = mid;
+        high = mid;
+        while (lp < low) {
+          int lmid = lp + (low - lp) / 2;
+          if (arr[lmid] < a) {
+            lp = lmid + 1;
+          } else if (arr[lmid] == a) {
+            low = lmid;
+          }
+        }
+        while (rp > high) {
+          int rmid = high + (rp - high) / 2 + 1;
+          if (arr[rmid] > a) {
+            rp = rmid - 1;
+          } else if (arr[rmid] == a) {
+            high = rmid;
+          }
+        }
+        break;
       }
     }
-    // return -1;
+    System.out.println("low:" + low + ", high:" + high);
   }
 }
---
+++ spring-web/src/main/java/com/edwin/spring/web/algorithm/assertion/AssertTest.java
@@ -0,0 +1,20 @@
+package com.edwin.spring.web.algorithm.assertion;
+
+/**
+ * 断言
+ * 
+ * @author caojunming
+ *
+ */
+public class AssertTest {
+
+  /**
+   * VM:-ea
+   * 
+   * @param args
+   */
+  public static void main(String[] args) {
+    int a = 1, b = 2;
+    assert a > b : "nonono";
+  }
+}
