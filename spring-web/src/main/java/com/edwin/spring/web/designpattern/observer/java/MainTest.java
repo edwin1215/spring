7cc5dfa69407edfc48264f181931a2971b918081
edwin1215 <supercjm1215@163.com>
2017-12-11 10:58:13 +0800

    Twitter_Snowflake 算法

--- spring-web/src/main/java/com/edwin/spring/web/designpattern/observer/java/MainTest.java
+++ spring-web/src/main/java/com/edwin/spring/web/designpattern/observer/java/MainTest.java
@@ -1,5 +1,9 @@
 package com.edwin.spring.web.designpattern.observer.java;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -13,4 +17,13 @@ public class MainTest {
     };
 
     Observable observable = new Observable();
+
+    public static void main(String[] args) {
+        System.out.println(System.currentTimeMillis());
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Calendar c = Calendar.getInstance();
+        long startTime = 1420041600000L;
+        c.setTimeInMillis(2012958020734L + startTime);
+        System.out.println(df.format(c.getTime()));
+    }
 }
