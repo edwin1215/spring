fc5ed319a843118e98cd39ddc31947e9fc2d31ca
caojunming <junming.cao@qunar.com>
2021-12-04 15:40:52 +0800

    实现延时队列

--- spring-web/src/main/java/com/edwin/spring/web/queue/delay/DelayCell.java
+++ spring-web/src/main/java/com/edwin/spring/web/queue/delay/DelayCell.java
@@ -2,10 +2,10 @@ package com.edwin.spring.web.queue.delay;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.concurrent.Delayed;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
@@ -46,6 +46,21 @@ public class DelayCell<T> implements Delayed {
 
     @Override
     public int compareTo(Delayed o) {
-        return 0;
+        if (o == this) // compare zero if same object
+            return 0;
+        if (o instanceof DelayCell) {
+            DelayCell<?> x = (DelayCell<?>)o;
+            long diff = timestamp - x.timestamp;
+            if (diff < 0)
+                return -1;
+            else if (diff > 0)
+                return 1;
+            else if (sequenceNumber < x.sequenceNumber)
+                return -1;
+            else
+                return 1;
+        }
+        long diff = getDelay(MILLISECONDS) - o.getDelay(MILLISECONDS);
+        return (diff < 0) ? -1 : (diff > 0) ? 1 : 0;
     }
 }
